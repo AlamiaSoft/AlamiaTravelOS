82e0559e5a93966b1809fe3d1cfd5705e039a28d
amrshah <amr.shah@gmail.com>
2026-09-07 14:11:22 +0500

    clean: remove temporary view check scripts

--- scripts/test_view.py
+++
@@ -1,6 +0,0 @@
-v_id = env.ref('alamia_travel_finance.view_travel_sub_agent_kanban').id
-res = env['res.partner'].get_views([(v_id, 'kanban')])
-print('--- VIEW ARCH ---')
-print(res['views']['kanban']['arch'])
-print('--- FIELDS IN VIEW ---')
-print(list(res['views']['kanban']['fields'].keys()))
